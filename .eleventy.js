1cd5286d613e8938ae759fd622923eecf53efda7
rylandc <rylandc@users.noreply.github.com>
2020-03-10 17:51:27 -0600

    js passthrough copy

--- .eleventy.js
+++ .eleventy.js
@@ -19,5 +19,6 @@ module.exports = function(eleventyConfig) {
     });
     
     eleventyConfig.addPassthroughCopy("styles");
+    eleventyConfig.addPassthroughCopy("scripts");
     eleventyConfig.addPassthroughCopy("images");
 };
